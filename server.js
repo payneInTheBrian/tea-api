6ef7d6b62c1f55974d00f74a80f27adb9bdf1cd7
iplaydrumsonmyguitar <gadomski.brian@gmail.com>
2022-06-12 23:08:34 -0500

    working tea api

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+const tea = {
+        'oolong':{
+            'type': 'black',
+            'origin': 'To mommas HOuse',
+            'waterTemp': 200,
+            'steepTime': 180,
+            'caffinated': true,
+            'flavor': 'delicious'
+        },
+        'unknown':{
+            'type': 'unkown',
+            'origin': 'unknown',
+            'waterTemp': 'unknown',
+            'steepTime': 'unknown',
+            'caffinated': 'false',
+            'flavor': 'unknown'
+        },
+        'matcha':{
+            'type': 'green',
+            'origin': 'To mommas HOuse',
+            'waterTemp': 200,
+            'steepTime': 180,
+            'caffinated': true,
+            'flavor': 'delicious'
+        }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const teaName = request.params.name.toLowerCase()
+    if( tea[teaName] ){
+        response.json(tea[teaName])
+    }else{
+        response.json(tea['unknown'])
+    }
+    response.json(tea)
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on port ${PORT}! Betta Go Catch It!`)
+})
